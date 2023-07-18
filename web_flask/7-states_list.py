736173f826196a66ef24e21e77b56787e4e6a2a5
root <Caramonh@yahoo.com>
2023-07-18 10:16:39 -0700

    8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Script that starts a Flask web app"""
+#!/usr/bin/python3
+""" starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 
@@ -7,17 +9,14 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """tears down new session"""
+def teardown(exception):
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all("State")
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('states.html', states=sorted_states)
-
+    return render_template('7-states_list.html', states=states.values())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
