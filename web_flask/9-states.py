647cabe0aca2b4853324085e2198a3e29e989859
root <Caramonh@yahoo.com>
2023-07-19 08:49:24 -0700

    pep8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
-"""
+"""Starts a Flask web app"""
 from flask import Flask
 from flask import render_template
 from models import storage
@@ -11,64 +10,43 @@ if __name__ == '__main__':
 
     @app.route('/', strict_slashes=False)
     def index():
-        """Display 'Hello HBNB!'
-        """
+        """Display 'Hello HBNB!'"""
         return 'Hello HBNB!'
 
     @app.route('/hbnb', strict_slashes=False)
     def hbnb():
-        """Display 'HBNB'
-        """
+        """Display 'HBNB'"""
         return 'HBNB'
 
     @app.route('/c/<text>', strict_slashes=False)
     def c(text):
-        """Display “C ” followed by the value of
-        the text variable (replace underscore _
-        symbols with a space)
-        """
         return 'C ' + text.replace('_', ' ')
 
     @app.route('/python/')
     @app.route('/python/<text>', strict_slashes=False)
     def python(text="is cool"):
-        """Display “Python ”, followed by the value of
-        the text variable (replace underscore _
-        symbols with a space )
-        """
         return 'Python ' + text.replace('_', ' ')
 
     @app.route('/number/<int:n>', strict_slashes=False)
     def number(n):
-        """Display “n is a number” only if n is an integer
-        """
         return str(n) + ' is a number'
 
     @app.route('/number_template/<int:n>', strict_slashes=False)
     def number_template(n):
-        """Display a HTML page only if n is an integer
-        """
         return render_template('5-number.html', n=n)
 
     @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
     def number_odd_or_even(n):
-        """Display a HTML page only if n is an integer
-        """
         parity = 'even' if n % 2 == 0 else 'odd'
         return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
     @app.route('/states_list', strict_slashes=False)
     def states_list():
-        """Display a HTML page of the States
-        """
         states = storage.all(State).values()
         return render_template('7-states_list.html', states=states)
 
     @app.route('/cities_by_states', strict_slashes=False)
     def cities_by_states():
-        """Display a HTML page of the States and the
-        Cities by State
-        """
         states = storage.all(State).values()
         cities = list()
 
@@ -81,15 +59,11 @@ if __name__ == '__main__':
 
     @app.route('/states', strict_slashes=False)
     def states():
-        """Display a HTML page of all States
-        """
         states = storage.all(State).values()
         return render_template('7-states_list.html', states=states)
 
     @app.route('/states/<id>', strict_slashes=False)
     def get_state_by_uuid(id):
-        """Display a HTML page of a State and their cities
-        """
         states = storage.all(State).values()
 
         for state in states:
@@ -101,8 +75,6 @@ if __name__ == '__main__':
 
     @app.teardown_appcontext
     def teardown_db(error):
-        """Closes the database again at the end of the request.
-        """
         storage.close()
 
     app.run('0.0.0.0')
